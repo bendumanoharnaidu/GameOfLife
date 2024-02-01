c6d546ddb95d03cfdb8473b475665862c6536e81
bendu.naidu_ftc <bendumanoharnaidu@gmail.com>
2024-02-01 22:26:44 +0530

    Added Cell Entity

---
+++ src/main/java/org/example/Cell.java
@@ -0,0 +1,40 @@
+package org.example;
+public class Cell {
+    private CellStatus status;
+    public Cell(CellStatus status) {
+        this.status = status;
+    }
+    public CellStatus getStatus() {
+        return this.status;
+    }
+    public int countLiveNeighbors(int row, int col, Cell[][] cells) {
+        int count = 0;
+        for (int r = row-1; r <= row+1; r++) {
+            for (int c = col-1; c <= col+1; c++) {
+                if (r>=0 && r<cells.length && c>=0 && c<cells[0].length && !(r==row && c==col) && cells[r][c].status == CellStatus.ALIVE) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void evolve(int livingNeighbors) {
+        if (status == CellStatus.ALIVE) {
+            if (livingNeighbors<2 || livingNeighbors>3) {
+                status = CellStatus.DEAD;
+            }
+            else {
+                status = CellStatus.ALIVE;
+            }
+        }
+        else {
+            if (livingNeighbors == 3) {
+                status = CellStatus.ALIVE;
+            }
+            else {
+                status = CellStatus.DEAD;
+            }
+        }
+    }
+}
---
+++ src/test/java/org/example/CellTest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CellTest {
+    @Test
+    public void TestAliveCellWithOneNeighborDies() {
+        Cell cell = new Cell(CellStatus.ALIVE);
+        cell.evolve(1);
+        assertEquals(cell.getStatus(), CellStatus.DEAD);
+    }
+    @Test
+    public void TestAliveCellWithTwoNeighborsLives() {
+        Cell cell = new Cell(CellStatus.ALIVE);
+        cell.evolve(2);
+        assertEquals(cell.getStatus(), CellStatus.ALIVE);
+    }
+    @Test
+    public void TestDeadCellWithThreeNeighborsLives() {
+        Cell cell = new Cell(CellStatus.DEAD);
+        cell.evolve(3);
+        assertEquals(cell.getStatus(), CellStatus.ALIVE);
+    }
+    @Test
+    public void TestDeadCellWithTwoNeighborsStaysDead() {
+        Cell cell = new Cell(CellStatus.DEAD);
+        cell.evolve(2);
+        assertEquals(cell.getStatus(), CellStatus.DEAD);
+    }
+
+    @Test
+    public void TestCountLivingNeighbors() {
+        Cell[][] cells = new Cell[3][3];
+        cells[0][0] = new Cell(CellStatus.ALIVE);
+        cells[0][1] = new Cell(CellStatus.ALIVE);
+        cells[0][2] = new Cell(CellStatus.ALIVE);
+        cells[1][0] = new Cell(CellStatus.ALIVE);
+        cells[1][1] = new Cell(CellStatus.ALIVE);
+        cells[1][2] = new Cell(CellStatus.ALIVE);
+        cells[2][0] = new Cell(CellStatus.ALIVE);
+        cells[2][1] = new Cell(CellStatus.ALIVE);
+        cells[2][2] = new Cell(CellStatus.ALIVE);
+        int expected = 8;
+        int actual = cells[1][1].countLiveNeighbors(1,1, cells);
+        assertEquals(expected, actual);
+    }
+
+}
